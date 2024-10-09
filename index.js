3a05758bfe2f42d3a8895f2e6128d41008f66e20
hubalichanna <channahubali95@gmail.com>
2024-10-09 23:26:41 +0500

    final commit

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const methodOverride =require("method-override");
 
 const { v4: uuidv4 } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'))
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -51,6 +52,27 @@ app.get("/posts/:id", (req, res) => {
   let post = posts.find((p) => id === p.id);
   res.render("show.ejs", { post });
 });
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    let newContent=req.body.content;
+    let post =posts.find((p)=> id === p.id);
+    post.content=newContent;
+    console.log (post);
+    res.redirect("/posts")
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post =posts.find((p)=> id === p.id);
+    res.render("edit.ejs",{post});
+})
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    posts=posts.filter((p)=>id!==p.id);
+    res.redirect("/posts");
+})
 
 app.listen(port, () => {
   console.log("Listening port : 8080");
